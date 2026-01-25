59edb89d61241563a19000d170cc150ac5204bd0
David Santiago Martínez López <155002917+dsmartinezlopez@users.noreply.github.com>
2026-01-25 11:25:16 -0500

    Add files via upload

---
+++ SQLQueryProyectoEcommerceBrazil.sql
@@ -0,0 +1,43 @@
+
+-- Etapa de exploración - preguntas de negocio
+
+-- ANÁLISIS CON VISIÓN OPERATIVA
+-- kpi's:
+
+-- FULFILLMENT RATE
+
+--preguntas claves:
+
+--1. ¿Cuál fue el volumen de pedidos enviados y cuántos cumplieron el tiempo de entrega y cuántos no?
+--2. ¿Cuál es el GAP de incumplimiento de entregas comparando MoM (Month-over-Month) de cada año?
+--3. Si la clasificación de los productos se establece de acuerdo al peso, donde SMALL se denomina todo lo que pese <= 1000 gramos,
+--	 MEDIUM todo lo que pese > 1000 gramos y <= 10000 gramos, y BIG todo lo que pese > 10000 gramos ¿cuales fueron las clasificaciones
+--	 que más porcentaje de incumplimiento tuvieron ordenadas de forma descendente?
+--4. ¿Cuál fue el TOP 5 de órdenes_id enviadas que más se demoraron en completarse? muestre id de la orden, producto(s), 
+--	 ciudad origen (vendedor), ciudad destino (comprador) y cantidad de días transcurridos desde la fecha de compra y la fecha de entrega. 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- ANÁLISIS CON VISIÓN DE NEGOCIO/REVENUE
+-- kpi's:
+
+-- TOTAL DE PAGOS
+
+--preguntas claves:
+
+--1. ¿Cuál fue la suma del total de pagos que se cerró al primer día de cada mes? muestre el total acumulado de los pagos MoM (Month-over-Month)
+--2. ¿Cómo fue la distribución de los medios de pago para cada categoría de productos? muestre la composición porcentual para cada medio de pago por estado/provincia
+
+-- UTILIDADES
+
+--preguntas claves:
+
+--1. ¿Cuáles fueron el TOP 3 de ciudades por cada estado que más utilidades obtuvieron? 
+
+-- FLUJO DE CAJA
+
+--preguntas claves:
+
+--1. ¿Con qué medio de pago se registró la compra más alta que se pagó de contado y en qué fecha se ejecutó la compra? 
+--2. ¿Cuál fue el TOP 5 de vendedores que más flujo de caja obtuvieron de sus ventas? 
+
